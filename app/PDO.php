8b922c53f8fe97a2398098bcffa0ff2a09bd811e
NAOKIYANAGAWA <73929004+NAOKIYANAGAWA@users.noreply.github.com>
2021-02-12 10:37:04 +0900

    Update PDO.php

--- app/PDO.php
+++ app/PDO.php
@@ -6,14 +6,14 @@ class PDO {
   public $db;
 
   public function __construct() {
-    $dbhost = 'dbinstance.cy31igwz0jl1.us-east-2.rds.amazonaws.com';
+    $dbhost = '';
     $dbport = '3306';
     $dbname = 'ec_website';
     $charset = 'utf8' ;
     
     $dsn = "mysql:host=$dbhost;port=$dbport;dbname=$dbname;charset=$charset";
     $username = 'naoki';
-    $password = 'ronaldo7';
+    $password = '';
     try {
       $this->db = new \PDO($dsn, $username, $password);
     } catch (\PDOException $e) {
